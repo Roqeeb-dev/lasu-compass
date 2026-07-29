68808ec748a767e160e75dbe79c5671e09d2a71b
Roqeeb <shafiriyuroqeeb@gmail.com>
2026-07-29 15:55:11 +0100

    FIX: fixed the error in the siteurl in the layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-const siteUrl = "https://lasu-compass-ai.vercel.app/";
+const siteUrl = "https://lasu-compass.vercel.app/";
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
